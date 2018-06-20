6552c02eaee146a120a6a9d66441dfb7f161853c
Justine Koa <justinekoa@gmail.com>
2018-06-20 02:48:35 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,37 @@
 #write out your code here
 
 def least_coins(cents)
+  answer = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+  if cents % 25 == 0
+    answer[:quarters] = cents/25
+    return answer
+  else
+    remainder = answer % 25
+    num_quarters = (answer / 25).to_i
+    answer[:quarters] = num_quarters
+    cents = remainder
 
-#Code your answer here!
+  if cents % 10 == 0
+    answer[:dimes] = cents/10
+    return answer
+  else
+    remainder = answer % 10
+    num_dimes = (answer/10).to_i
+    answer[:dimes] = num_dimes
+    cents = remainder
+
+  if cents % 5 == 0
+    answer[:nickels] = cents/5
+    return answer
+  else
+    remainer = answer%5
+    num_nickels = (answer/5).to_i
+    answer[:nickels] = num_nickels
+    cents = remainder
+
+  answer[:pennies] = cents
+  return answer
 
 end
+
+puts least_coins(29)
